1662c24df1985e8edd9178d709237e58064a1295
Victor Borzov <borzov.vk@protonmail.com>
2024-08-22 13:13:24 +0200

    Added libsolution runtime task

--- src/libsolution-runtime/solution.c
+++ src/libsolution-runtime/solution.c
@@ -7,6 +7,13 @@ int stringStat(const char *string, size_t multiplier, int *count) {
 }
 
 
-int main(void) {
-  puts("urmom\n");
+int main(int argc, char** args) {
+  if (argc != 4) {
+    puts("Expected 3 arguments");
+    return 1;
+  }
+  char* libName = args[1];
+  char* funcName = args[2];
+  int param = (int)args[3];
+  printf("libname: %s\nfunc: %s\nparam: %i\n", libName, funcName, param);
 }
