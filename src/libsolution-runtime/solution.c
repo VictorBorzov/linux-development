e9e9971162c957369394f4e702316a03145e0907
Victor Borzov <borzov.vk@protonmail.com>
2024-08-23 17:25:45 +0200

    libsolution runtime finished

--- src/libsolution-runtime/solution.c
+++ src/libsolution-runtime/solution.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <dlfcn.h>
 
-int stringStat(const char *string, size_t multiplier, int *count) {
-  (*count)++;
-  return strlen(string) * multiplier;
+int (*lib_call)(int);
+
+bool init_library(char* lib_name, char* func_name, int param) {
+  void* handle = dlopen(lib_name, RTLD_LAZY);
+  if (NULL == handle) {
+    printf("Could not load handle %s\n", lib_name);
+    return false;
+  }
+
+  lib_call = (int (*)(int))dlsym(handle, func_name);
+  if (NULL == lib_call) {
+    return false;
+  }
+
+  int result = lib_call(param);
+  printf("%i\n", result);
+
+  return true;
 }
-
 
 int main(int argc, char** args) {
   if (argc != 4) {
     puts("Expected 3 arguments");
     return 1;
   }
-  char* libName = args[1];
-  char* funcName = args[2];
-  int param = (int)args[3];
-  printf("libname: %s\nfunc: %s\nparam: %i\n", libName, funcName, param);
+  if (init_library(args[1], args[2], atoi(args[3]))) {
+    return 0;
+  }
+
+  puts("Library not loaded");
+  return 1;
 }
