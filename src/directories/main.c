7fe4e1c90285ffa41b73b5593d0f9d95a668b465
Victor Borzov <borzov.vk@protonmail.com>
2024-08-24 11:20:47 +0200

    sandbox

--- src/directories/main.c
+++ src/directories/main.c
@@ -3,19 +3,22 @@
 
 
 void print_help(char** args) {
-  printf("Usage: %s <directory>\n", args[0]);
+  printf("Usage: %s [SUBCOMMAND] [OPTIONS] <directory>\n", args[0]);
+  printf("OPTIONS:\n");
+  printf("    cat <file>       print file content\n");
+  printf("    ls <directory>   list directory\n");
 }
 
 char* get_message() {
-  char* msg = "hello";
+  char *msg = "hello";
   return msg;
 }
 
 int main(int argsn, char** args) {
-  char* s = get_message();
+  char *s = get_message();
   puts(s);
 
- if (argsn != 2) {
+ if (argsn < 2) {
     print_help(args);
     return 1;
   }
